fad1604688f080f5cd8106f2c48e117768f090ed
iznue <lej010320@naver.com>
2023-08-17 01:33:51 +0900

    question_1 html form task

--- mbti/forms.py
+++ mbti/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired
 
 # E&I question answer
 class Question_1_Form(FlaskForm):
-    comment = TextAreaField('comment', validators=[DataRequired()])
+    content = TextAreaField('content', validators=[DataRequired()])
 
 # S&N question answer
 class Question_2_Form(FlaskForm):
-    comment = TextAreaField('comment', validators=[DataRequired()])
+    content = TextAreaField('content', validators=[DataRequired()])
--- mbti/views/test_views.py
+++ mbti/views/test_views.py
@@ -1,25 +1,27 @@
+from datetime import datetime
 from flask import Blueprint, render_template, request, url_for
 from mbti.forms import Question_1_Form, Question_2_Form
 from werkzeug.utils import redirect
+
+from mbti import db
+from mbti.models import E_I_answer, S_N_answer
 
 # MBTI test 진행
 bp = Blueprint('test', __name__, url_prefix='/test') 
 
 ##########################################################################
 
-@bp.route('/question_1', methods=['GET','POST'])
+@bp.route('/question_1')
 def E_I_question():
     form = Question_1_Form()
-
-    print(request.method)
-    print(form.validate_on_submit())
-
-    if request.method == 'POST' and form.validate_on_submit():
-        if request.form['comment'] == 'hi':
-            print('ok')
-        else:
-            return redirect(url_for('question_2'))
     return render_template('test.html', form=form)
+
+@bp.route('/question_1_create', methods=['GET','POST'])
+def E_I_create():
+    content = request.form['content']
+    comment = E_I_answer(content=content, create_date=datetime.now())
+    print('ok')
+    return comment
 
 ###########################################################################
 @bp.route('/question_2', methods=['GET','POST'])
