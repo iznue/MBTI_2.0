932e48c05d6bbe8ad0e0ed6dea8eb95513f61d0e
iznue <lej010320@naver.com>
2023-08-17 18:47:25 +0900

    set session

--- mbti/__init__.py
+++ mbti/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +10,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    app.secret_key = 'mbti_2.0' # session 사용을 위한 키 설정
 
     db.init_app(app)
     migrate.init_app(app, db)
--- mbti/views/test_views.py
+++ mbti/views/test_views.py
@@ -1,9 +1,8 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, session
 from mbti.forms import Question_1_Form, Question_2_Form
 from werkzeug.utils import redirect
 from keras.preprocessing.sequence import pad_sequences
-from mbti import db
 from mbti.models import E_I_answer, S_N_answer
 
 import torch
@@ -12,9 +11,9 @@ from pytorch_transformers import BertTokenizer, BertForSequenceClassification
 # MBTI test 진행
 bp = Blueprint('test', __name__, url_prefix='/test') 
 
-Mbti_pred ={}
+# Mbti_pred ={}
 
-# Model
+# Models
 ##########################################################################
 
 device = torch.device("cpu")
@@ -30,15 +29,16 @@ model_2.eval()
 ##########################################################################
 @bp.route('/question_1')
 def E_I_question():
+    session.clear() # 웹브라우저 방문(사용자마다) session 초기화
     return render_template('test.html')
 
 # E & I 추론 작업
 @bp.route('/question_1', methods=['GET','POST'])
 def E_I_predict():
     data_1 = request.form['comment_1'] # test.html의 form key 값을 받아옴
-    Mbti_pred['E&I'] = data_1
     print(data_1)
-    return render_template('test_2.html', data=data_1) # test_2.html에 추론 결과 전달
+    session['E&I'] = 'E'
+    return render_template('test_2.html') # test_2.html에 추론 결과 전달
 
 # S & N
 ###########################################################################
@@ -65,8 +65,10 @@ def S_N_predict():
     pred_2 = torch.argmax(output, 1)
     
     predict_2 = class_name[pred_2]
-    Mbti_pred['S&N'] = predict_2
-    print(Mbti_pred)
+
+    session['S&N'] = predict_2 # 추론 결과를 session에 value값으로 저장함
+    # Mbti_pred['S&N'] = predict_2
+    print(session)
     return render_template('test_3.html')
 
 # T & F
@@ -74,6 +76,7 @@ def S_N_predict():
 # T & F 추론 작업
 @bp.route('/question_3', methods=['GET','POST'])
 def T_F_predict():
+    session['T&F'] = 'T'
     return render_template('test_4.html')
 
 
@@ -82,4 +85,6 @@ def T_F_predict():
 # P & J 추론 작업
 @bp.route('/question_4', methods=['GET','POST'])
 def P_J_predict():
+    session['P&J'] = 'J'
     return render_template('result.html')
+    # result.html에서 session 값을 통해 사용자의 mbti 검사 결과를 출력함
